a0be179995f311a614abdd7305351fc23b2b7f63
Ajay Sahani <sajay3159@gmail.com>
2025-10-02 17:45:26 +0530

    Loading the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
 import React, { useState } from "react";
-
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchMoviesHandler = async () => {
-    const response = await fetch("https://swapi.info/api/films");
-    const data = await response.json();
+    setIsLoading(true);
+    try {
+      const response = await fetch("https://swapi.info/api/films");
+      const data = await response.json();
 
-    const transformedMovie = data.map((movieData) => ({
-      id: movieData.episode_id,
-      title: movieData.title,
-      openingText: movieData.opening_crawl,
-      releaseDate: movieData.release_date,
-    }));
+      const transformedMovie = data.map((movieData) => ({
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      }));
 
-    setMovies(transformedMovie);
+      setMovies(transformedMovie);
+    } catch (error) {
+      console.error("Failed to fetch movies:", error);
+    }
+    setIsLoading(false);
   };
 
   return (
@@ -26,7 +32,8 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {isLoading && <p>Loading...</p>}
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
       </section>
     </React.Fragment>
   );
